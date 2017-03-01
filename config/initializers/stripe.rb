a98d63c5445cb7480d1b8d946c22279d949a878c
Puff <puff.tw+github@gmail.com>
2017-03-01 07:21:48 +0000

    update stript config use figaro setting.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,8 @@
 Rails.configuration.stripe = {
-  # :publishable_key => ENV['PUBLISHABLE_KEY'],
-  # :secret_key      => ENV['SECRET_KEY']
-  :publishable_key => Rails.configuration.payment["stripe"]["public_key"],
-  :secret_key      => Rails.configuration.payment["stripe"]["private_key"]
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  # :publishable_key => Rails.configuration.payment["stripe"]["public_key"],
+  # :secret_key      => Rails.configuration.payment["stripe"]["private_key"]
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
-
