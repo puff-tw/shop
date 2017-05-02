894a120ee5ec59478d5b4c9c5206ceca946fd52a
Puff <puff.tw+github@gmail.com>
2017-05-02 02:35:24 +0000

    configured app to handle emails in devlopment update

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address              => 'smtp.gmail.com',
+  :port                 => 587,
+  :authentication       => 'plain',
+  :user_name            => ENV[SMTP_USERNAME],
+  :password             => ENV[SMTP_PASSWORD],
+  :enable_starttls_auto => true
+}
