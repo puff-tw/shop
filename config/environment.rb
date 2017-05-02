4f5c23d83c83fb31a47c5d5507de08e7429af6e9
Puff <puff.tw+github@gmail.com>
2017-05-02 03:17:52 +0000

    fix typo

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
   :address              => 'smtp.gmail.com',
   :port                 => 587,
   :authentication       => 'plain',
-  :user_name            => ENV[SMTP_USERNAME],
-  :password             => ENV[SMTP_PASSWORD],
+  :user_name            => ENV['SMTP_USERNAME'],
+  :password             => ENV['SMTP_PASSWORD'],
   :enable_starttls_auto => true
 }
