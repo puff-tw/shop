abc88ac7cf69b51bf8c1cd48cf02145856478a62
Puff <puff.tw+github@gmail.com>
2017-05-03 07:22:54 +0000

    Display Product Information from the Database

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-
+    @product = Product.find_by_sku("pf001")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503064246) do
+ActiveRecord::Schema.define(version: 20170503071049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170503064246) do
     t.decimal  "price"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "info"
   end
 
   create_table "purchases", force: :cascade do |t|
