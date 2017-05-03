b7a71f542e1cd8e96b57bb8c53bbabc7f90c427c
Puff <puff.tw+github@gmail.com>
2017-05-03 06:46:57 +0000

    added the product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503055336) do
+ActiveRecord::Schema.define(version: 20170503064246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,19 @@ ActiveRecord::Schema.define(version: 20170503055336) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.string   "image_name"
+    t.string   "sku"
+    t.string   "download_url"
+    t.string   "details"
+    t.text     "description"
+    t.decimal  "price"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "purchases", force: :cascade do |t|
